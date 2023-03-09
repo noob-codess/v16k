c650397511dd166e85df58b2f4313f29a1217432
noob-codess <viswanadhashashank.us@gmail.com>
2023-03-09 01:22:43 -0600

    Added typing text 2

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const typingText = document.getElementById('typing-text');
 const text = "Experienced DevOps System Engineer with a focus on results who has worked in the fields of software configuration management, quality assurance, DevOps, build and release management for many years. Significant proficiency with Amazon Web Services (AWS), and Azure Infrastructure, Devops engineering, development and configuration, scripting tools, and system development tools are among the core competencies. Excellent technical troubleshooting abilities with system software and web platforms, experience previously assisting and working with system monitoring teams, DevOps engineers, and software development teams.";
 let index = 0;
 
+
+const typingText2 = document.getElementById('typing-text2');
+const text2 = "If you have any project idea around DevOps you can reach me out for collaboration";
+let index2 = 0;
+
+
 function type() {
   if (index < text.length) {
     typingText.innerHTML += text.charAt(index);
@@ -9,6 +15,15 @@ function type() {
     setTimeout(type, 50);
   }
 }
+
+function type() {
+  if (index2 < text.length) {
+    typingText2.innerHTML += text2.charAt(index2);
+    index2++;
+    setTimeout(type, 50);
+  }
+}
+
 
 type();
 
